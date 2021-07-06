682be512e6795be06a8edbf87b20d13b3394bf34
hlib.b <hlib.b@advabet.com>
2021-07-07 00:32:25 +0300

    added IndexHome component

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,29 +1,18 @@
 <template>
   <div id="app">
-    <div class="main">
-      <div class="main__wrapper">
-        <div class="main__header">
-          <img
-            src="@/assets/img/logo.svg"
-            width="300"
-            height="47"
-            alt="V!U!E! Pizza"
-          />
-        </div>
-        <h1>Добро пожаловать!</h1>
-        <p>
-          Это проект V!U!E! Pizza для обучения на профессиональном
-          онлайн‑курсе<br />
-          <b>«Vue.js для опытных разработчиков».</b>
-        </p>
-      </div>
-    </div>
+    <IndexHome />
   </div>
 </template>
 
 <script>
+import IndexHome from "@/views/Index";
+
 export default {
   name: "App",
+
+  components: {
+    IndexHome,
+  },
 };
 </script>
 
---
+++ src/frontend/src/views/Index.vue
@@ -0,0 +1,189 @@
+<template>
+  <div>
+    <header class="header">
+      <div class="header__logo">
+        <a href="index.html" class="logo">
+          <img
+            src="@/assets/img/logo.svg"
+            alt="V!U!E! Pizza logo"
+            width="90"
+            height="40"
+          />
+        </a>
+      </div>
+      <div class="header__cart">
+        <a href="cart.html">0 ₽</a>
+      </div>
+      <div class="header__user">
+        <a href="#" class="header__login"><span>Войти</span></a>
+      </div>
+    </header>
+
+    <main class="content">
+      <form action="#" method="post">
+        <div class="content__wrapper">
+          <h1 class="title title--big">Конструктор пиццы</h1>
+
+          <div class="content__dough">
+            <div class="sheet">
+              <h2 class="title title--small sheet__title">Выберите тесто</h2>
+
+              <div class="sheet__content dough">
+                <label
+                  v-for="(el, i) in dough"
+                  :key="`dough-${i}`"
+                  class="dough__input dough__input--light"
+                >
+                  <input
+                    type="radio"
+                    name="dought"
+                    value="light"
+                    class="visually-hidden"
+                    checked
+                  />
+                  <b>{{ el.name }}</b>
+                  <span>{{ el.description }}</span>
+                </label>
+              </div>
+            </div>
+          </div>
+
+          <div class="content__diameter">
+            <div class="sheet">
+              <h2 class="title title--small sheet__title">Выберите размер</h2>
+
+              <div class="sheet__content diameter">
+                <label
+                  v-for="(size, i) in sizes"
+                  :key="`size-${i}`"
+                  class="diameter__input diameter__input--small"
+                >
+                  <input
+                    type="radio"
+                    name="diameter"
+                    value="small"
+                    class="visually-hidden"
+                  />
+                  <span>{{ size.name }}</span>
+                </label>
+              </div>
+            </div>
+          </div>
+
+          <div class="content__ingridients">
+            <div class="sheet">
+              <h2 class="title title--small sheet__title">
+                Выберите ингридиенты
+              </h2>
+
+              <div class="sheet__content ingridients">
+                <div class="ingridients__sauce">
+                  <p>Основной соус:</p>
+
+                  <label
+                    v-for="(sauce, i) in sauces"
+                    :key="`sauce-${i}`"
+                    class="radio ingridients__input"
+                  >
+                    <input type="radio" name="sauce" value="tomato" checked />
+                    <span>{{ sauce.name }}</span>
+                  </label>
+                </div>
+
+                <div class="ingridients__filling">
+                  <p>Начинка:</p>
+
+                  <ul class="ingridients__list">
+                    <li
+                      v-for="(ingredient, i) in ingredients"
+                      :key="`ingredient-${i}`"
+                      class="ingridients__item"
+                    >
+                      <span
+                        :class="['filling', `filling--${ingredient.class}`]"
+                      >
+                        {{ ingredient.name }}
+                      </span>
+
+                      <div class="counter counter--orange ingridients__counter">
+                        <button
+                          type="button"
+                          class="
+                            counter__button
+                            counter__button--disabled
+                            counter__button--minus
+                          "
+                        >
+                          <span class="visually-hidden">Меньше</span>
+                        </button>
+                        <input
+                          type="text"
+                          name="counter"
+                          class="counter__input"
+                          value="0"
+                        />
+                        <button
+                          type="button"
+                          class="counter__button counter__button--plus"
+                        >
+                          <span class="visually-hidden">Больше</span>
+                        </button>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="content__pizza">
+            <label class="input">
+              <span class="visually-hidden">Название пиццы</span>
+              <input
+                type="text"
+                name="pizza_name"
+                placeholder="Введите название пиццы"
+              />
+            </label>
+
+            <div class="content__constructor">
+              <div class="pizza pizza--foundation--big-tomato">
+                <div class="pizza__wrapper">
+                  <div class="pizza__filling pizza__filling--ananas"></div>
+                  <div class="pizza__filling pizza__filling--bacon"></div>
+                  <div class="pizza__filling pizza__filling--cheddar"></div>
+                </div>
+              </div>
+            </div>
+
+            <div class="content__result">
+              <p>Итого: 0 ₽</p>
+              <button type="button" class="button button--disabled" disabled>
+                Готовьте!
+              </button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </main>
+  </div>
+</template>
+
+<script>
+import pizza from "@/static/pizza.json";
+
+export default {
+  name: "IndexHome",
+
+  data() {
+    return {
+      sizes: pizza.sizes,
+      dough: pizza.dough,
+      sauces: pizza.sauces,
+      ingredients: pizza.ingredients,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
